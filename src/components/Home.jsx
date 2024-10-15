b5ff4af68b5b7884c487b0361cfd13dcdd344fee
SaidSafwan <saidsafwan59@gmail.com>
2024-10-15 22:30:26 +0530

    Update home Page with Increase section size with new slogan and background Image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 const Home = () => {
     return (
-        <section className="flex items-center justify-center h-full max-h-[calc(100vh-112px)] bg-white p-8 rounded-lg shadow-lg transform transition duration-500 hover:scale-105">
-            <div className="text-center">
-                <h1 className="text-3xl font-bold mb-4">Welcome to Our Advanced Hospital Management System</h1>
-                <p className="text-gray-700">Efficient healthcare management at your fingertips</p>
+        <section className="flex items-center justify-center h-full min-h-[calc(300px)] bg-white bg-cover bg-center p-8 rounded-lg shadow-lg transform transition duration-500 hover:scale-105"
+        style={{ backgroundImage: "url('/Images/Home-Bg-Image.jpg')", opacity: 0.9 }}
+        >
+            <div className="text-center mt-auto mb-3">
+                <h1 className="text-3xl font-bold mb-1 text-white">Your Health, Our Commitment: A New Era of Care</h1>
+                <p className="text-grey ">Simplifying your journey to better health.</p>
             </div>
         </section>
     );
 };
 
 export default Home;
+

e015381c3138e5c9d9e4c1f6fecadc3f1e9ca4e4
rohitja26 <jainrohit0002@gamil.com>
2024-10-16 18:33:30 +0530

    Added the changes required in the footer i.e. media icons and some footer conventionals

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,8 +1,47 @@
+import { RxTwitterLogo } from "react-icons/rx";
+import { PiFacebookLogo } from "react-icons/pi";
+import { RxInstagramLogo } from "react-icons/rx";
+import { PiLinkedinLogo } from "react-icons/pi";
 const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center mt-4 fixed w-full bottom-0">
-            <p className="text-sm">&copy; 2024 Hospital Management System. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer className="  bg-gray-800 text-white p-4 mt-4 fixed w-full bottom-0">
+      <div className="flex justify-center  items-center space-x-24 mb-4 text-lg">
+        <a href="#" className="hover:underline pr-8">
+          About
+        </a>
+        <a href="#" className="hover:underline pl-8 ">
+          Contact
+        </a>
+        <a href="#" className="hover:underline pl-12">
+          Privacy Policy
+        </a>
+        <a href="#" className="hover:underline">
+          Terms of Service
+        </a>
+        <a href="#" className="hover:underline">
+          Accessibility
+        </a>
+      </div>
+
+      <div className="flex justify-center items-center space-x-4 mb-4 ">
+        <a href="#" className="hover:text-blue-600 text-lg">
+          <RxTwitterLogo />
+        </a>
+        <a href="#" className="hover:text-blue-700 text-lg">
+          <PiFacebookLogo />
+        </a>
+        <a href="#" className="hover:text-pink-500 text-lg">
+          <RxInstagramLogo />
+        </a>
+        <a href="#" className="hover:text-blue-700 text-lg">
+          <PiLinkedinLogo />
+        </a>
+      </div>
+
+      <div className="text-sm flex justify-center items-center ">
+        &copy; 2024 Hospital Management System. All rights reserved.
+      </div>
+    </footer>
+  );
 };
-export default Footer
+export default Footer;
